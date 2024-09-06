88cc6978e10b1609955cf6654525cea5b73c03b8
maajidmuhammad <abdimohamed0742@gmail.com>
2024-09-06 17:48:45 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
-    pass
+    extension= ".txt"
+    string =f"{file_name}{extension}"
+    with open(string, mode="w", encoding="utf-8") as text_file:
+        text_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+     extension= ".txt"
+     string =f"{file_name}{extension}"
+     with open(string, mode="a", encoding="utf-8") as text_file:
+         text_file.write(append_content)
 
 def read_file(file_name):
-    pass
+     extension= ".txt"
+     string =f"{file_name}{extension}"
+     with open(string,mode="r",encoding="utf-8") as text_file:
+         return text_file.read()
